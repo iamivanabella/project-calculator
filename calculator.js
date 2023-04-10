fb84798050c89dbb4b880c15d8a35055cb5fd7bf
iamivanabella <abella.ivan18@gmail.com>
2023-04-11 04:29:52 +0800

    Adding project calculator

---
+++ calculator.js
@@ -0,0 +1,67 @@
+let firstNum = null, secondNum = null, operator = null;
+const result = document.getElementById("result");
+const total = document.getElementById("total");
+const clear = document.getElementById("clear");
+const oper = document.getElementById("oper");
+const numbers = document.querySelectorAll(".number");
+const btnOperators = document.querySelectorAll(".operator");
+
+clear.onclick = () => {
+    firstNum = null;
+    secondNum = null; 
+    operator = null;
+    result.value = 0;
+    oper.innerHTML = operator;
+};
+
+function operate(x, y, op) {
+    if (x && y) {
+        switch (op) {
+            case "/":
+                return result.value = x / y;
+            case "*":
+                return result.value = x * y;
+            case "-":
+                return result.value = x - y;
+            case "+":
+                return result.value = Number(x) + Number(y);
+            default:
+                break;
+        }
+    }
+    
+}
+
+const appendNumber = (num, clickedNum) => (num ? num + clickedNum : clickedNum);
+
+numbers.forEach(number => {
+    number.onclick = function () {
+            if (!operator) {
+                firstNum = appendNumber(firstNum, number.textContent);
+                result.value = firstNum;
+            }else {
+                secondNum = appendNumber(secondNum, number.textContent);
+                result.value = secondNum;
+            }
+    }
+});
+
+btnOperators.forEach(btnOperator => {
+    btnOperator.onclick = function () {
+        if (operator) {
+            firstNum = operate(firstNum, secondNum, operator);
+            secondNum = null;
+        } 
+        btnOperators.forEach(btnOperator => btnOperator.style.backgroundColor = "rgb(246, 139, 0)");
+        this.style.backgroundColor = "rgb(249 176 81)";
+        operator = btnOperator.textContent;
+        oper.innerHTML = operator;
+    }
+});
+
+total.onclick = () => {
+    firstNum = operate(firstNum, secondNum, operator);
+    secondNum = null;
+    operator = null;
+    oper.innerHTML = operator;
+};
